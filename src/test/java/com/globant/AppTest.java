03a189fd38b5b3871638ce15ad89fd67ad3e994b
Felipe Rivas <fe.rivasb@gmail.com>
2022-11-07 19:58:35 -0500

    First commit

---
+++ src/test/java/com/globant/AppTest.java
@@ -0,0 +1,17 @@
+package com.globant;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void fuckThis(){
+        System.out.println("god fucking damnit");
+    }
+}
