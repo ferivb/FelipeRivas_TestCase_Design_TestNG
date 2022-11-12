5eb0c769915fb215d3f1aca3e3e2262854d0f7a5
Felipe Rivas <fe.rivasb@gmail.com>
2022-11-12 17:44:01 -0500

    Done: Listener Class. Next: WebOperations class

--- src/main/java/com/globant/reporting/Reporter.java
+++ src/main/java/com/globant/reporting/Reporter.java
@@ -1,4 +1,25 @@
 package com.globant.reporting;
 
-public class Reporter {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class Reporter {
+
+    public Reporter(){}
+
+    public static Logger getLogger() {
+        return LoggerFactory.getLogger(Reporter.class); // Get this explained
+    }
+
+    public static void info(String text){
+        getLogger().info(text);
+    }
+
+    public static void error(String text){
+        getLogger().error(text);
+    }
+
+    public static void warning(String text){
+        getLogger().warn(text);
+    }
 }
--- src/test/java/com/globant/utils/Listener.java
+++ src/test/java/com/globant/utils/Listener.java
@@ -1,4 +1,45 @@
 package com.globant.utils;
 
-public class Listener {
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import com.globant.reporting.Reporter;
+
+public class Listener implements ITestListener {
+
+    @Override
+    public void onTestSuccess(ITestResult result){
+        Reporter.info("Test: " + result.getName() + " [PASSED]");
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result){
+        Reporter.error("Test: " + result.getName() + " [FAILED]");
+    }
+
+    @Override
+    public void onTestStart(ITestResult iTestResult) {
+
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult iTestResult) {
+
+    }
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
+
+    }
+
+    @Override
+    public void onStart(ITestContext iTestContext) {
+
+    }
+
+    @Override
+    public void onFinish(ITestContext iTestContext) {
+
+    }
+
 }
